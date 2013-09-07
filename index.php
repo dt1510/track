fd25810a0795ac95da7413b10af4b3f94baa1709
David Toth <dt1510@ic.ac.uk>
2013-09-07 09:20:00 -0700

    reset.css added.

--- index.php
+++ index.php
@@ -8,15 +8,70 @@ function rglob($pattern='*', $flags = 0, $path='')
     foreach ($paths as $path) { $files=array_merge($files,rglob($pattern, $flags, $path)); }
     return $files;
 }
+
+function get_tracking_files() {
+    global $base_dir;
+    return rglob("meta.txt", 0, $base_dir);
+}
+
+function subject_name($meta_file) {
+    $names = explode("/", $meta_file);
+    return $names[count($names)-2];
+}
+
+
+function extract_list($meta_file) {
+    $list = array();
+    $content = file_get_contents($meta_file);
+    $blocks = preg_split("/\n(\n)+/", $content);
+    foreach($blocks as $block) {
+        $sublist = preg_split("/\n/", $block);
+        $heading = $sublist[0];
+        unset($sublist[0]);
+        $list[$heading]=$sublist;        
+    }
+    return $list;
+}
+
+function print_list($list) {
+
+    foreach($list as $heading => $sublist) {
+        echo "<div>";
+        echo "<sublist_header>$heading</sublist_header>";
+        echo "<ul>";
+        foreach($sublist as $item) {
+            echo "<li>$item</li>";
+        }
+        echo "</ul>";
+        echo "</div>";
+    }
+}
+
+function print_subject($file) {
+    echo "<subject>".subject_name($file)."</subject>";
+    print_list(extract_list($file));
+}
+
 ?>
 
 <html>
+<head>
+<link rel="stylesheet" href="reset.css" type="text/css">
+</head>
 <body>
-<h1>Personal management tracking</h1>
 <?
-var_export(rglob("todo.txt", 0, $base_dir));
+foreach(get_tracking_files() as $file) {
+    print_subject($file);
+}
 
 ?>
 
 </body>
+<style>
+body{padding:3px;}
+div{padding:3px;}
+h1{font-weight:bold;}
+sublist_header{font-weight:bold;}
+subject{font-weight:bold;}
+</style>
 </html>
